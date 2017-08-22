c744e685b286cd9821d4cdede106782099527c14
Sagar Desai <sagar.desai@uoit.net>
2017-08-21 22:03:04 -0400

    Added schemes from the unixporn colorscheme contest winners

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 /*
- * Author: Sagar Desai
+ * Author: Probotect0r
  * Description: Purpose of this project is to randomly generate a full theme for linux
  * environment based on a color shceme. 
  * This includes generating themes for multiple different applications and applying them.
+ * The color schemes are mostly taken from /r/unixporn community.
  */
 
 const { apps, appsConf, brightness } = require('./config.js')
